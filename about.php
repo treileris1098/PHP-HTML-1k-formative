04aa330ae98bb3cd04ae5be417c8105dbbbe6bbb
You <you@example.com>
2025-12-05 09:04:33 +0200

    3uzd

--- about.php
+++ about.php
@@ -1,1 +1,19 @@
 <!-- your code goes here -->
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+ </head>
+ <body>
+    <?php
+
+echo "<h1>about<\h1>";
+$indexedArray = [suns,keķis,abols]
+
+
+
+?>
+ </body>
+ </html>
